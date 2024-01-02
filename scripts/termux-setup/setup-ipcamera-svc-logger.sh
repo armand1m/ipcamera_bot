a87bb90049043781f69e4624b94d095b58ee8d5b
Armando Magalhaes <armando.mag95@gmail.com>
2024-01-02 01:08:24 +0100

    refactor: setup scripts

--- scripts/termux-setup/setup-ipcamera-svc-logger.sh
+++ scripts/termux-setup/setup-ipcamera-svc-logger.sh
@@ -1,10 +1,10 @@
 #!/data/data/com.termux/files/usr/bin/sh
 export PKG=ipcamera_bot
 
-mkdir -p $LOGDIR/sv/ipcamera_bot
+mkdir -p $LOGDIR/sv/$PKG
 mkdir -p $SVDIR/$PKG/log
 cat > $SVDIR/$PKG/log/run <<EOL
 #!/data/data/com.termux/files/usr/bin/sh
-exec svlogd -v -tt "$LOGDIR/sv/ipcamera_bot"
+exec svlogd -v -tt "$LOGDIR/sv/$PKG"
 EOL
 chmod +x $SVDIR/$PKG/log/run
--- scripts/termux-setup/setup-ipcamera-svc-runner.sh
+++ scripts/termux-setup/setup-ipcamera-svc-runner.sh
@@ -1,9 +1,10 @@
 #!/data/data/com.termux/files/usr/bin/sh
 export PKG=ipcamera_bot
+
 cat > $SVDIR/$PKG/run <<EOL
 #!/data/data/com.termux/files/usr/bin/sh
-cd /data/data/com.termux/files/home/Projects/ipcamera_bot
-exec ./target/release/ipcamera_bot 2>&1
+cd /data/data/com.termux/files/home/Projects/$PKG
+exec ./target/release/$PKG 2>&1
 EOL
 chmod +x $SVDIR/$PKG/run
 
